e7eab7937d4a060a90bbe1fb7162a3de3b505c92
igorjohn <89204391+igorjohn@users.noreply.github.com>
2023-01-30 19:06:00 -0300

    front update

--- src/views/Checkout/Checkout.vue
+++ src/views/Checkout/Checkout.vue
@@ -1,18 +1,19 @@
 <script setup>
 
 import { ref } from 'vue'
-import { TransitionRoot, TransitionChild, Dialog, DialogPanel } from '@headlessui/vue'
+import { TransitionRoot, TransitionChild, Dialog, DialogPanel, Listbox, ListboxButton, ListboxOptions, ListboxOption } from '@headlessui/vue'
+import { ChevronDownIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
 
 let openLoader = ref(false);
 function onPurchase() {
     openLoader.value = true;
 }
-
 function closeLoader() {
     openLoader.value = false;
 }
 
 let isOpenDiscountCoupon = ref(false);
+
 
 const installment = [
     { installments: 1, text: '1x R$ 197,00 (à vista)' },
@@ -27,7 +28,8 @@ const installment = [
     { installments: 10, text: '10x R$ 25,68' },
     { installments: 11, text: '11x R$ 24,04' },
     { installments: 12, text: '12x R$ 22,70' }
-]
+];
+const selectedInstallment = ref(installment[11]);
 
 </script>
 
@@ -36,6 +38,7 @@ const installment = [
 // Mock images
 import BannerImg from '@/assets/img/mock/checkout-banner.png';
 import OrderBump01 from '@/assets/img/mock/orderbump-01.jpeg';
+import OrderBump02 from '@/assets/img/mock/orderbump-02.jpeg';
 
 export default {
     data() {
@@ -43,7 +46,6 @@ export default {
             pepper: this.pepper,
             tab: 1,
             checkoutHas2Columns: true,
-            selectedInstallment: 12,
 
             Elements: {
                 Badges: [
@@ -79,7 +81,6 @@ export default {
                     ]
                 },
 
-
             },
 
             Classes: {
@@ -112,13 +113,15 @@ export default {
                     secondaryText: 'block text-xs text-gray-500 mb-4 font-medium'
                 },
                 orderBump: {
-                    label: 'w-full flex flex-col grow-1 rounded-md border-2 border-dashed border-red-600 hover:border-red-500 hover:shadow-lg transition duration-500 cursor-pointer',
+                    label: 'w-full flex flex-col grow-1 rounded-md mb-2 border-2 border-dashed border-red-600 hover:border-red-500 hover:shadow-lg transition duration-500 cursor-pointer',
                     titleWrapper: 'w-full bg-gray-200 flex items-center justify-center gap-1 px-3 py-2.5 rounded-t-md',
-                    title: 'text-slate-900 text-base tracking-tight font-bold',
-                    bodyWrapper: 'bg-slate-50 px-3 pt-4 pb-5 lg:px-4 flex gap-3 flex-row items-start rounded-b',
+                    title: 'text-slate-900 text-base tracking-tight font-bold italic',
+                    bodyWrapper: 'bg-zinc-50 px-3 pt-4 pb-5 lg:px-4 flex gap-3 flex-row items-start rounded-b',
                     checkbox: 'w-4 h-4 lg:w-5 lg:h-5 mt-[2px] rounded orderbump-checkbox ring-0 outline-none focus:ring-0 focus:ring-offset-0',
                     image: 'aspect-square block rounded w-12 h-12 object-cover shadow-lg',
-
+                    h1: 'block text-slate-800 text-base tracking-tight font-bold -mt-[3px]',
+                    p: 'block text-slate-600 text-sm font-medium mt-1',
+                    price: 'block text-slate-500 text-xs font-medium mt-2'
                 }
             },
 
@@ -194,7 +197,37 @@ export default {
                         <path d="m596.39 279.65c-0.0625-3.1133-1.4961-6.1758-3.8516-8.2227l-156.8-134.33c-3.2383-2.8125-8.1562-3.5117-12.051-1.7148-3.8945 1.7969-6.5469 5.9961-6.5 10.285v67.164h-302.39c-5.8633 0-11.199 5.332-11.199 11.195v111.94c0 5.8594 5.3359 11.191 11.199 11.195h302.39v67.164c-0.042968 4.2852 2.6055 8.4844 6.5 10.285 3.8945 1.7969 8.8125 1.0977 12.051-1.7148l156.8-134.32c2.5312-2.1953 3.9883-5.5742 3.8516-8.918z"></path>
                     </g>
                 </svg>`,
-            }
+            },
+
+            OrderBumpProducts: [
+                {
+                    productId: 10012,
+                    img: OrderBump01,
+                    hasCustomText: true,
+                    text: {
+                        h1: 'Seja um Chef Foda!',
+                        p: 'Compre este curso e aprenda todas as receitas que você sempre quis. Para amadores e profissionais que desejam aprender dotes culinários e crescer seu negócio.'
+                    },
+                    price: {
+                        fake: 'R$ 899,90',
+                        real: 'R$ 97,00'
+                    }
+                },
+                {
+                    productId: 10532,
+                    img: OrderBump02,
+                    hasCustomText: true,
+                    text: {
+                        h1: 'Conquiste uma nova profissão, com renda de até R$ 8.998,97',
+                        p: 'Aprenda tudo que você precisa para se tornar o eletricista que o mercado precisa.'
+                    },
+                    price: {
+                        fake: 'R$ 346,90',
+                        real: 'R$ 72,00'
+                    }
+                },
+
+            ]
         }
     },
     methods: {
@@ -349,9 +382,18 @@ export default {
                         <label :class="Classes.label">
                             Ano:
                         </label>
-                        <div :class="Classes.containerInputIcon">
-                            <input :class="Classes.input" required inputmode="text" type="text" />
-                        </div>
+                        <select required :class="[Classes.input,]" class="disabled:opacity-40">
+                            <option selected disabled>Ano</option>
+                            <option value="2023">2023</option>
+                            <option value="2024">2024</option>
+                            <option value="2025">2025</option>
+                            <option value="2026">2026</option>
+                            <option value="2027">2027</option>
+                            <option value="2028">2028</option>
+                            <option value="2029">2029</option>
+                            <option value="2030">2030</option>
+                            <option value="2031">2031</option>
+                        </select>
                     </div>
 
                     <div class="col-span-1">
@@ -368,11 +410,37 @@ export default {
                         <label :class="Classes.label">
                             Parcelamento:
                         </label>
-                        <select v-model="selectedInstallment" :class="Classes.input">
-                            <template v-for="inst in installment">
-                                <option :value="inst.installments">{{ inst.text }}</option>
-                            </template>
-                        </select>
+                        <Listbox v-model="selectedInstallment">
+                            <div class="relative mt-1">
+                                <ListboxButton
+                                    class="relative text-start" :class="Classes.input">
+                                    <span class="block truncate">{{ selectedInstallment.text }}</span>
+                                    <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                                        <ChevronDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+                                    </span>
+                                </ListboxButton>
+                                <transition
+                                    leave-active-class="transition duration-100 ease-in"
+                                    leave-from-class="opacity-100"
+                                    leave-to-class="opacity-0">
+                                    <ListboxOptions
+                                        class="absolute mt-1 w-full rounded-md bg-white overflow-hidden shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                        <ListboxOption
+                                            v-slot="{ active, selected }"
+                                            v-for="inst in installment"
+                                            :key="inst.installments"
+                                            :value="inst"
+                                            as="template">
+                                            <li :class="[active ? 'bg-blue-100 text-blue-900' : 'text-slate-800', 'relative cursor-pointer select-none py-1.5 px-4']">
+                                                <span :class="[selected ? 'font-semibold' : 'font-normal', 'block truncate text-[13px] lg:text-sm',]">
+                                                    {{ inst.text }}
+                                                </span>
+                                            </li>
+                                        </ListboxOption>
+                                    </ListboxOptions>
+                                </transition>
+                            </div>
+                        </Listbox>
                     </div>
                 </div>
                 <!-- End Credit card Tab -->
@@ -418,30 +486,31 @@ export default {
                 <div class="lg:col-span-2 my-2">
 
                     <!-- Order bump -->
-                    <label :class="Classes.orderBump.label">
-                        <span :class="Classes.orderBump.titleWrapper">
-                            <div v-html="icons.orderBumpArrow"></div>
-                            <span :class="Classes.orderBump.title">
-                                Sim, quero levar também!
+                    <template v-for="ob in OrderBumpProducts">
+                        <label :class="Classes.orderBump.label">
+                            <span :class="Classes.orderBump.titleWrapper">
+                                <div v-html="icons.orderBumpArrow"></div>
+                                <span :class="Classes.orderBump.title">
+                                    Sim, quero levar também!
+                                </span>
                             </span>
-                        </span>
-
-                        <div :class="Classes.orderBump.bodyWrapper">
-                            <input type="checkbox" :class="Classes.orderBump.checkbox" />
-                            <img :src="OrderBump01" :class="Classes.orderBump.image" />
-                            <div>
-                                <span class="block text-slate-800 text-base tracking-tight font-bold -mt-[3px]">
-                                    Seja um Chef Foda!
-                                </span>
-                                <span class="block text-slate-600 text-sm font-medium mt-1">
-                                    Compre este curso e aprenda todas as receitas que você sempre quis. Para amadores e profissionais que desejam aprender dotes culinários e crescer seu negócio.
-                                </span>
-                                <span class="block text-slate-500 text-xs font-medium mt-2">
-                                    De <span class="line-through">R$ 899,90</span> por R$ 97,00
-                                </span>
+                            <div :class="Classes.orderBump.bodyWrapper">
+                                <input type="checkbox" :class="Classes.orderBump.checkbox" />
+                                <img :src="ob.img" :class="Classes.orderBump.image" />
+                                <div>
+                                    <span :class="Classes.orderBump.h1">
+                                        {{ ob.text.h1 }}
+                                    </span>
+                                    <span :class="Classes.orderBump.p">
+                                        {{ ob.text.p }}
+                                    </span>
+                                    <span :class="Classes.orderBump.price">
+                                        De <span class="line-through">{{ ob.price.fake }}</span> por {{ ob.price.real }}
+                                    </span>
+                                </div>
                             </div>
-                        </div>
-                    </label>
+                        </label>
+                    </template>
                     <!-- End Order bump -->
                 </div>
 
@@ -586,6 +655,41 @@ html,
 
 svg {
     max-width: 100% !important;
+}
+
+.vs--unsearchable .vs__search {
+    opacity: 0;
+    padding: 0;
+}
+
+.v-select,
+.vs__dropdown-toggle {
+    height: 36px;
+}
+
+.vs__actions {
+    padding: 0.25rem 0.55rem 0;
+    transform: scale(0.75);
+}
+
+.vs--single .vs__selected {
+    margin-top: 0;
+    padding-top: 0;
+    padding-bottom: 0;
+    height: 36px;
+}
+
+.v-select * {
+    font-size: small;
+}
+
+.v-select ul li:not(:hover) {
+    color: #676767;
+}
+
+.vs__dropdown-toggle {
+    margin: 0;
+    border-radius: .35rem;
 }
 
 #buyBtn {
