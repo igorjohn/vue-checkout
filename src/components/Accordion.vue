43a5db9493edcb76d8138f60d274503f1c544dcb
igorjohn <89204391+igorjohn@users.noreply.github.com>
2023-02-03 21:56:19 -0300

    added - accordion component

---
+++ src/components/Accordion.vue
@@ -0,0 +1,23 @@
+
+<script>
+import AccordionItem from "./AccordionItem";
+
+export default {
+    components: {
+        AccordionItem,
+    },
+    props: {
+        questions: Array,
+    },
+};
+</script>
+  
+<template>
+    <section class="info">
+        <AccordionItem
+            v-for="question in questions"
+            :key="question.id"
+            :title="question.title"
+            :info="question.info" />
+    </section>
+</template>
