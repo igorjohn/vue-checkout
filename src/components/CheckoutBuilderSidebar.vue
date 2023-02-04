b9051138ae7fad020f96ddc1a1db949b5348e1db
igorjohn <89204391+igorjohn@users.noreply.github.com>
2023-02-03 21:56:36 -0300

    added - checkout builder sidebar component

---
+++ src/components/CheckoutBuilderSidebar.vue
@@ -0,0 +1,212 @@
+<script setup>
+import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/vue/20/solid'
+</script>
+
+
+<script>
+export default {
+    data() {
+        return {
+
+            tab: 'TabMain',
+
+            SidebarMenu: [
+                {
+                    name: 'Imagem',
+                    tab: 'TabImage',
+                    svg: `
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                    </svg>`
+                },
+                {
+                    name: 'Depoimentos',
+                    tab: 'TabTestimonials',
+                    svg: `
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                    </svg>`
+                },
+                {
+                    name: 'Order bumps',
+                    tab: 'TabOrderBumps',
+                    svg: `
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                    </svg>`
+                },
+                {
+                    name: 'Selos',
+                    tab: 'TabBadges',
+                    svg: `
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.491 4.491 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.491 4.491 0 01-1.307 3.497 4.491 4.491 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.491 4.491 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+                    </svg>`
+                },
+                {
+                    name: 'Lista',
+                    tab: 'TabList',
+                    svg: `
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                    </svg>`
+                },
+                {
+                    name: 'Notificações',
+                    tab: 'TabSalesNotifications',
+                    svg: `
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+                    </svg>`
+                },
+                {
+                    name: 'Vídeo do Youtube',
+                    tab: 'TabEmbedVideo',
+                    svg: `
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                        <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z" clip-rule="evenodd" />
+                    </svg>`
+                },
+                {
+                    name: 'Texto',
+                    tab: 'TabText',
+                    svg: `
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                    </svg>`
+                },
+                {
+                    name: 'Contagem regressiva',
+                    tab: 'TabCountdown',
+                    svg: `
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" stroke-width="2" viewBox="0 0 17 17">
+                        <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z"/>
+                        <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z"/>
+                    </svg>`
+                },
+                {
+                    name: 'WhatsApp',
+                    tab: 'TabWhatsapp',
+                    svg: `
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" stroke-width="1.5" viewBox="0 0 18 18">
+                        <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+                    </svg>`
+                },
+                {
+                    name: 'Back redirect',
+                    tab: 'TabBackRedirect',
+                    svg: `
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+                    </svg>`
+                }
+            ]
+        }
+    },
+
+    methods: {
+        changeTab(e) {
+            this.tab = e;
+        },
+    },
+
+};
+</script>
+  
+<template>
+
+    <div class="w-full z-10 max-w-[19rem] pt-4 pb-10 px-4 flex flex-col gap-4 bg-slate-50 border-r border-slate-200 fixed h-screen overflow-y-auto">
+
+        <div class="w-full flex flex-col gap-2 mx-auto">
+
+            <!-- Tab Menu -->
+            <div class="w-full flex flex-col gap-2 mx-auto" v-show="tab === 'TabMain'">
+                <template v-for="item in SidebarMenu">
+                    <div
+                        @click="changeTab(item.tab)"
+                        class="w-full flex items-center justify-between bg-gray-100 border border-slate-200 rounded-md px-3 py-3.5 cursor-pointer hover:shadow-lg hover:border-indigo-500 transition duration-700">
+                        <div class="flex items-center justify-start gap-2 grow">
+                            <div v-html="item.svg" class="w-5 h-5 inline-flex items-center text-slate-500"></div>
+                            <span class="font-semibold text-[15px] tracking-tight text-slate-600">{{ item.name }}</span>
+                        </div>
+                        <ChevronRightIcon class="w-5 h-5 text-indigo-500" />
+                    </div>
+                </template>
+            </div>
+
+            <!-- Back button -->
+            <div
+                class="flex items-center justify-start gap-2 text-sm text-indigo-500 font-semibold cursor-pointer mb-3"
+                v-if="tab !== 'TabMain'"
+                @click="changeTab('TabMain')">
+                <ChevronLeftIcon class="w-3.5 h-3.5" />
+                Voltar
+            </div>
+
+            <!-- Tab Image -->
+            <div class="w-full flex flex-col gap-2 mx-auto" v-show="tab === 'TabImage'">
+                Adicionar imagem
+            </div>
+
+            <!-- Tab Whatsapp -->
+            <div class="w-full flex flex-col gap-2 mx-auto" v-show="tab === 'TabWhatsapp'">
+                Whatsapp
+            </div>
+
+            <!-- Tab Testimonials -->
+            <div class="w-full flex flex-col gap-2 mx-auto" v-show="tab === 'TabTestimonials'">
+                Depoimentos
+            </div>
+
+            <!-- Tab Order bumps -->
+            <div class="w-full flex flex-col gap-2 mx-auto" v-show="tab === 'TabOrderBumps'">
+                Order Bumps
+            </div>
+
+            <!-- Tab Badges -->
+            <div class="w-full flex flex-col gap-2 mx-auto" v-show="tab === 'TabBadges'">
+                Selos
+            </div>
+
+            <!-- Tab List -->
+            <div class="w-full flex flex-col gap-2 mx-auto" v-show="tab === 'TabList'">
+                Adicionar lista
+            </div>
+
+            <!-- Tab Sales Notifications -->
+            <div class="w-full flex flex-col gap-2 mx-auto" v-show="tab === 'TabSalesNotifications'">
+                Notificações
+            </div>
+
+            <!-- Tab Youtube Video -->
+            <div class="w-full flex flex-col gap-2 mx-auto" v-show="tab === 'TabEmbedVideo'">
+                Adicionar vídeo do YouTube
+            </div>
+
+            <!-- Tab Text -->
+            <div class="w-full flex flex-col gap-2 mx-auto" v-show="tab === 'TabText'">
+                Adicionar texto
+            </div>
+
+            <!-- Tab Countdown -->
+            <div class="w-full flex flex-col gap-2 mx-auto" v-show="tab === 'TabCountdown'">
+                Contagem regressiva
+            </div>
+
+            <!-- Tab Whatsapp -->
+            <div class="w-full flex flex-col gap-2 mx-auto" v-show="tab === 'TabWhatsapp'">
+                Adicionar botão do WhatsApp
+            </div>
+
+            <!-- Tab Back Redirect -->
+            <div class="w-full flex flex-col gap-2 mx-auto" v-show="tab === 'TabBackRedirect'">
+                Back Redirect
+            </div>
+
+        </div>
+
+
+
+    </div>
+
+</template>
