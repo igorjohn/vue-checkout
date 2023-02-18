f0e69e96e08c7f88170600465329bc885d320fd6
igorjohn <89204391+igorjohn@users.noreply.github.com>
2023-02-18 18:03:31 -0300

    added component 'FormLabel'

---
+++ src/components/FormLabel.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    props: {
+        name: String
+    }
+}
+</script>
+
+<template>
+    <div class="block text-sm font-medium text-slate-600 mb-1">
+        {{ name }}
+    </div>
+</template>
